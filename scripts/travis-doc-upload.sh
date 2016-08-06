84fd9e0af19cd6e0c4df4976fae5357e34aec952
Brian Martin <brayniac@gmail.com>
2016-08-06 10:43:08 -0700

    fix autodoc (#12)

--- scripts/travis-doc-upload.sh
+++ scripts/travis-doc-upload.sh
@@ -21,7 +21,7 @@ eval key=\$encrypted_${SSH_KEY_TRAVIS_ID}_key
 eval iv=\$encrypted_${SSH_KEY_TRAVIS_ID}_iv
 
 mkdir -p ~/.ssh
-openssl aes-256-cbc -K $key -iv $iv -in scripts/travis.enc -out ~/.ssh/id_rsa -d
+openssl aes-256-cbc -K $key -iv $iv -in scripts/autodoc.enc -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
 
 git clone --branch gh-pages git@github.com:$DOCS_REPO deploy_docs
